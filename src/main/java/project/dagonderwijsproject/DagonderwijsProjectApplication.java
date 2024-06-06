e6abf2d43d96fb3665e84b638abfb6946d0a4b00
Ammar <ammarnajjar@gmail.com>
2024-06-06 13:23:06 +0200

    first commit

---
+++ src/main/java/project/dagonderwijsproject/DagonderwijsProjectApplication.java
@@ -0,0 +1,13 @@
+package project.dagonderwijsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DagonderwijsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DagonderwijsProjectApplication.class, args);
+	}
+
+}
